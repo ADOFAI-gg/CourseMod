4aa102b706cd9ff48f122ab94423445f0015f363
CrackThrough <me@cooki.dev>
2026-01-14 02:03:36 +0900

    style: ignore namespace rules

--- Assets/Scripts/Plugins/UiRoundedCorners/DestroyHelper.cs
+++ Assets/Scripts/Plugins/UiRoundedCorners/DestroyHelper.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+// ReSharper disable once CheckNamespace
 namespace Nobi.UiRoundedCorners {
 	internal static class DestroyHelper {
 		internal static void Destroy(Object @object) {
